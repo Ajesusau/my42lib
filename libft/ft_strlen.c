5502ee6d1fbaf4b016815b568f13478ee08b5605
Ajesusau <ajesusau@hotmail.com>
2025-06-18 12:17:28 +0200

    Actualizar la fecha de modificación en ft_strlen.c y mejorar la verificación de punteros nulos

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: anareval <anareval@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 12:40:25 by anareval          #+#    #+#             */
-/*   Updated: 2025/01/27 19:46:31 by anareval         ###   ########.fr       */
+/*   Updated: 2025/06/18 12:13:44 by anareval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ size_t	ft_strlen(const char *str)
 	size_t	length;
 
 	length = 0;
+	if (!str)
+		return (0);
 	while (*str)
 	{
 		length ++;
